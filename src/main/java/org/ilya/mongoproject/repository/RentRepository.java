2bc5d5df34bb22f17282b320a35dd575a51119a9
nawm77 <alpha050603@gmail.com>
2023-10-10 22:53:36 +0300

    add dockerfile, k8s deployment and service manifests

--- src/main/java/org/ilya/mongoproject/repository/RentRepository.java
+++ src/main/java/org/ilya/mongoproject/repository/RentRepository.java
@@ -4,7 +4,6 @@ import org.ilya.mongoproject.model.entities.Rent;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 import java.util.Optional;
 
 @Repository
