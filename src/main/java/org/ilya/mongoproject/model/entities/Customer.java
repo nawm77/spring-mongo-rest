ccdda0a536364800f93dad68803f34ac51cec29d
nawm77 <alpha050603@gmail.com>
2023-10-15 01:30:41 +0300

    remove unused import

--- src/main/java/org/ilya/mongoproject/model/entities/Customer.java
+++ src/main/java/org/ilya/mongoproject/model/entities/Customer.java
@@ -6,7 +6,6 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
-import org.springframework.data.mongodb.core.mapping.Field;
 
 @Document(collection = "users")
 @Data
